681f6dab48f3d23c5f4ed41ff9496d73ff961c41
Saran Tanpituckpong <sarunta@gmail.com>
2017-03-04 15:56:25 +0700

    Support GitHub mobile site

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-ruby-sass');
+const concat = require('gulp-concat');
 
 gulp.task('sass', () => {
 	return sass('./src/*.scss', { style: 'expanded' })
 			.on('error', sass.logError)
+			.pipe(concat('NoDarkMode.css'))
 			.pipe(gulp.dest('./extension'));
 });
 
 gulp.task('sass:min', () => {
 	return sass('./src/*.scss', { style: 'compressed' })
 			.on('error', sass.logError)
+			.pipe(concat('NoDarkMode.css'))
 			.pipe(gulp.dest('./extension'));
 });
 
